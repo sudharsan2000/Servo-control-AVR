b7853aeaf0d9ac103d20fd34dc5eab2442f90a0e
Sudharsan <ssudharsan2000@gmail.com>
2019-08-15 22:07:50 +0530

    Basic structure of pgm

--- Servo.ino
+++ Servo.ino
@@ -1,1 +1,48 @@
-#include<avr/io.h>
+#include <avr/io.h>
+
+Class servo
+{
+public :
+        void begin()
+        {
+            
+        }
+        void servo_write(int angle)
+        {
+            
+        }
+};
+class usart
+{
+    public:
+    void usart_init(unsigned long baud)
+    {
+        if (choice == 9600)
+        {
+            UCSR0A = 0X00;
+            UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+            UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+            UBRR0 = 103; //Set baud rate to 9600
+        }
+    }
+    void usart_print(String data)
+    {
+        for (int i = 0; data[i] != '\0'; i++)
+        {
+            while (!(UCSR0A & (1 << UDRE0)))
+                ;
+            UDR0 = data[i];
+        }
+    }
+    char usart_read()
+    {
+        while (!(UCSR0A & (1 << RXC0)))
+            ;
+        return UDR0;
+    }
+};
+
+int main()
+{
+    return 0;
+}
